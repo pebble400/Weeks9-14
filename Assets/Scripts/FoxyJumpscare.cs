2bff714da3f4f4b8f76e611b812b755afa45c8f9
Anita Hosseini <hosseani@shernet.sheridancollege.ca>
2025-03-20 14:56:28 -0400

    Started working on coding gym

---
+++ Assets/Scripts/FoxyJumpscare.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxyJumpscare : MonoBehaviour
+{
+    public AnimationCurve curve;
+    [Range(0, 1)]
+    public float t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        Vector2 pos = transform.localScale;
+        pos.x = curve.Evaluate(t);
+        pos.y = curve.Evaluate(t);
+    }
+}
