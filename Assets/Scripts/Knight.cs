1f504b389a9fe77afec0183265b8668cef055a95
Anita Hosseini <hosseani@shernet.sheridancollege.ca>
2025-03-27 14:02:33 -0400

    Did some animation work

---
+++ Assets/Scripts/Knight.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using UnityEngine;
+
+public class Knight : MonoBehaviour
+{
+    SpriteRenderer sr;
+    Animator animator;
+    public float speed = 2;
+    public bool canRun = true;
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = Input.GetAxis("Horizontal");
+
+        sr.flipX = (direction < 0);
+        animator.SetFloat("movement", Mathf.Abs(direction));
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("attack");
+            canRun = false;
+        }
+        
+        if(canRun == true)
+        {
+            transform.position += transform.right * direction * speed * Time.deltaTime;
+        }
+        
+    }
+
+    public void AttackHasFinished()
+    {
+        Debug.Log("The attack animation just finished!");
+        canRun = true;
+    }
+
+}
