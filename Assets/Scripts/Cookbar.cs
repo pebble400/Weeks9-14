7be1f624e865f541431123e80cab8dd8eb5d79a1
Anita Hosseini <hosseani@shernet.sheridancollege.ca>
2025-04-03 15:19:28 -0400

    Remade the cook bar

--- Assets/Scripts/Cookbar.cs
+++ Assets/Scripts/Cookbar.cs
@@ -5,19 +5,22 @@ using UnityEngine.UI;
 
 public class Cookbar : MonoBehaviour
 {
-    public float underCooked = 0.2f;
-    public float perfectCooked = 0.5f;
-    public float overCooked = 0.7f;
-    public Slider visuals;
-    float time = 0;
+    Slider slider;
+    float t;
+    //public float underCooked = 0.2f;
+    //public float perfectCooked = 0.5f;
+    //public float overCooked = 0.7f;
+    //public Slider visuals;
+    //float time = 0;
     void Start()
     {
-        
+        slider = GetComponent<Slider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        t += Time.deltaTime;
+        slider.value = t % slider.maxValue;
     }
 }
